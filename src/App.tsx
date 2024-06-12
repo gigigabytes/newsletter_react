ee967af791bfebcbffd44cc18b7033e7c0ed45bb
Giovanna <maria.giovanna@escolar.ifrn.edu.br>
2024-06-12 14:45:09 -0300

    feat: styles inside forms.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import Forms from './components/Forms'
+import Forms from "./components/Forms";
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-red-600-">Inscreva-se</h1>
-      <p>Assine nossa Newsletter</p>
-      <div>
-      <Forms />
-      </div>
-      <p>Ao se inscrever, você passará a receber nossos e-mails.</p>
-    </div>
-  )
+    return (
+        <div className="bg-slate-900 min-h-screen w-full flex flex-col items-center justify-center">
+            <h1 className="font-bold text-[2rem] text-white">Inscreva-se</h1>
+            <p className="text-white">Assine nossa Newsletter</p>
+            <div className="w-96 mt-4 bg-stone-200 px-4 py-5">
+                <Forms />
+            </div>
+            <p className="text-slate-100 text-xs w-96 mt-2 tex-center">Ao se inscrever, você passará a receber nossos e-mails.</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
